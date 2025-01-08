c281222fcf63d69d832684ad5275eee3a4d97000
Jimmyh091 <jaimecaballero164@gmail.com>
2025-01-08 15:46:09 +0100

    ej3 puto

--- main.py
+++ main.py
@@ -14,6 +14,10 @@ def filtrar_regalos(lista):
     return [x for x in lista if x[0] == 'R' or x[0] == 'r']
 
 
+def contar_bolas(lista):
+    return len([x for x in lista if x >= 5])
+
 
 imprimir_arbol(50)
 print(filtrar_regalos(["rafslkd", "skfdjha", "RsRkfdjae", "JKHkjfdase"]))
+print(contar_bolas([4, 6,7 ,8,2, 2, 2,2, 7, 2, 2, 7, 2, 2, 26, 6, 27, 7823, 3]))
