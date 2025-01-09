59d4ee716775e448fe3df2f64eb0135e395e76ac
Jimmyh091 <jaimecaballero164@gmail.com>
2025-01-09 17:43:26 +0100

    era intento del ej6

--- main.py
+++ main.py
@@ -60,12 +60,61 @@ def sec_natal(lista):
     return resultado
 
 
+def comprobar_filas(tablero):
+
+    for fila in tablero:
+
+        for it in range(1, len(fila)):
+            if fila[it] == fila[it-1]:
+                return False
+
+    return True
+
+
+def comprobar_columnas(tablero):
+
+    for it in range(0, len(tablero)):
+
+        for it2 in range(1, len(tablero)):
+            if tablero[it][it2] == tablero[it][it2-1]:
+                return False
+
+    return True
+
+def comprobar_cuadros(tablero):
+
+    for itBig in range(0, len(tablero) / 3 - 1):
+        for it2Big in range(0, len(tablero[0]) / 3 - 1):
+
+            for itSmall in range(0, 2):
+                for it2Small in range(0, 2):
+
+                    tablero[itBig * 3 + itSmall][it2Big * 3 + it2Small] = 3
+
+def resolver_sudoku(tablero):
+
+    print("Resolviendo sudoku")
+
+    tableroCopia = tablero.copy()
+
+
 imprimir_arbol(50)
 print(filtrar_regalos(["rafslkd", "skfdjha", "RsRkfdjae", "JKHkjfdase"]))
 print(contar_bolas([4, 6, 7, 8, 2, 2, 2, 2, 7, 2, 2, 7, 2, 2, 26, 6, 27, 7823, 3]))
 print(sorteo_navidad(["Jiame", "Jefferson", "Jimena", "JoJo"], ["GOTY", "Goya", "galardon de navidad"]))
 cuenta_regresiva(50)
-listststst = sec_natal([["Jaime", 19], ["Gaspar", 103], ["Carlos", 14], ["Fran", 23], ["David", 29], ["Nacho", 17], ["Nahuel", 24]])
+print(sec_natal([["Jaime", 19], ["Gaspar", 103], ["Carlos", 14], ["Fran", 23], ["David", 29], ["Nacho", 17], ["Nahuel", 24]]))
 
-for it in listststst:
-    print(it)
+resolver_sudoku(
+    [
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    ]
+)
